6236953697a771ff2a00f9fbc9c4b3d460d46af8
aasthha0421 <justaasthaa@gmail.com>
2025-07-14 21:16:15 +0530

    Fix: Add protect middleware to /api/mood/add route

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -14,6 +14,9 @@ const protect = async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
+       if (!req.user) {
+        return res.status(401).json({ message: "User not found" });
+      }
 
       next(); // pass control to the next middleware/route
     } catch (error) {
@@ -22,9 +25,6 @@ const protect = async (req, res, next) => {
     }
   }
 
-  if (!token) {
-    return res.status(401).json({ message: "Not authorized, no token" });
-  }
 };
 
 module.exports = protect;
